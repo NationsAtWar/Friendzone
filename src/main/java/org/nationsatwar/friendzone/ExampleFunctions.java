004c5f7507d6ecbfb6685ceb8c59ffb63ea94ff0
Matt Luesing <Kainte@gmail.com>
2015-05-23 21:49:38 -0700

    Removed useless function

--- src/main/java/org/nationsatwar/friendzone/ExampleFunctions.java
+++ src/main/java/org/nationsatwar/friendzone/ExampleFunctions.java
@@ -1,16 +1,7 @@
 package org.nationsatwar.friendzone;
 
-import org.nationsatwar.friendzone.buckets.Bucket;
-import org.nationsatwar.friendzone.buckets.BucketAPI;
-
 public class ExampleFunctions {
 	
 	// Nothing in this class is actually ever called
 	// These are just examples of how you would create various setups
-	
-	// This is how a party would 
-	public void createParty() {
-		
-		Bucket bucket = BucketAPI.createBucket("UniquePartyID");
-	}
 }
